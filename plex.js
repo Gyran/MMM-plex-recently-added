fc0a443735bf0b6f27c270f6b4f01e494a368469
Gustav Ahlberg <Gustav.Ahlberg@gmail.com>
2017-03-11 04:00:00 +0100

    use plex api with extra headers correctly

--- plex.js
+++ plex.js
@@ -17,16 +17,19 @@ module.exports = (config) => {
     var typeNum = TYPE_MAP[type];
 
     return new Promise(function (resolve, reject) {
-      client.query('/hubs/home/recentlyAdded?type=' + typeNum + '&X-Plex-Container-Start=0&X-Plex-Container-Size=' + limit)
+      client.query({
+        'uri': '/hubs/home/recentlyAdded?type=' + typeNum,
+        'extraHeaders': {
+          'X-Plex-Container-Start': '0',
+          'X-Plex-Container-Size': limit.toString(),
+        },
+      })
         .then(function (result) {
           var data = result.MediaContainer.Metadata
             .filter((item) => item.type === type);
 
           resolve(data);
-        }, function (error) {
-          console.log(':(', error);
-          reject(error);
-        });
+        }, reject);
     });
   }
 
