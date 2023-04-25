06678ddf3827ff4486a8d5fc05a3fe4500733f0b
Gustav Ahlberg <Gustav.Ahlberg@gmail.com>
2023-04-25 23:35:41 +0200

    feat: fetch all item once and send what the client wants

--- node_helper.js
+++ node_helper.js
@@ -20,8 +20,7 @@ const Types = {
 
 const PlexTypes = {
   [Types.MOVIE]: '1',
-  [Types.EPISODE]: '2',
-  [Types.SEASON]: '2',
+  [Types.TV]: '2',
 };
 
 const fetchData = (url) => {
@@ -88,21 +87,34 @@ module.exports = NodeHelper.create({
     }, Math.max(delayMs, TEN_MINUTES_MS));
   },
 
-  process() {
-    for (const type of this.config.types) {
-      this.fetchFromPlex(type);
+  async process() {
+    try {
+      const plexItemsByType = await Promise.all(
+        Object.values(PlexTypes).map((plexType) =>
+          this.fetchFromPlex(plexType),
+        ),
+      );
+      const items = plexItemsByType.flat();
+
+      for (const type of this.config.types) {
+        const typeItems = items.filter((item) => item.type === type);
+        this.sendSocketNotification(Notifications.DATA, {
+          type,
+          items: typeItems.slice(0, this.config.limit),
+        });
+      }
+    } catch (error) {
+      Log.error(error);
+      this.sendSocketNotification(Notifications.ERROR, {
+        error: error.message,
+      });
     }
 
     // schedule the next fetch
     this.scheduleNextFetch(this.config.updateIntervalInMinute * ONE_MINUTE_MS);
   },
 
-  async fetchFromPlex(type) {
-    const plexType = PlexTypes[type];
-    if (plexType === undefined) {
-      throw new Error(`Can not determine plex type for type '${type}'`);
-    }
-
+  async fetchFromPlex(plexType) {
     const url = new URL(
       `http://${this.config.hostname}:${this.config.port}/hubs/home/recentlyAdded`,
     );
@@ -113,33 +125,21 @@ module.exports = NodeHelper.create({
 
     Log.info(`${this.name}: fetching ${url}`);
 
-    try {
-      const data = await fetchData(url);
+    const data = await fetchData(url);
 
-      const now = new Date();
-      let newerThanDate =
-        this.config.newerThanDay > 0
-          ? new Date(now.getTime() - this.config.newerThanDay * ONE_DAY_MS)
-          : new Date(0);
+    const now = new Date();
+    let newerThanDate =
+      this.config.newerThanDay > 0
+        ? new Date(now.getTime() - this.config.newerThanDay * ONE_DAY_MS)
+        : new Date(0);
 
-      const items = data.MediaContainer.Metadata.filter(
-        (item) => item.type === type,
-      ).filter((item) => {
-        const itemAddedDate = new Date(item.addedAt * 1000);
+    const items = data.MediaContainer.Metadata.filter((item) => {
+      const itemAddedDate = new Date(item.addedAt * 1000);
 
-        return itemAddedDate >= newerThanDate;
-      });
+      return itemAddedDate >= newerThanDate;
+    });
 
-      Log.info(`${this.name}: found ${items.length} items for type ${type}`);
-      this.sendSocketNotification(Notifications.DATA, {
-        type,
-        items: items.slice(0, this.config.limit),
-      });
-    } catch (error) {
-      Log.error(error);
-      this.sendSocketNotification(Notifications.ERROR, {
-        error: error.message,
-      });
-    }
+    Log.info(`${this.name}: found ${items.length} items for type ${plexType}`);
+    return items;
   },
 });
